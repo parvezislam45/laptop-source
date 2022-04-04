d576a136d6cb5374d1a0e8e9aa1a078509cbd84f
parvezislam <parvezislam45@gmail.com>
2022-04-04 16:46:47 +0600

    add chirt

--- src/components/DeshBoard/DeshBoard.js
+++ src/components/DeshBoard/DeshBoard.js
@@ -1,9 +1,91 @@
 import React from 'react';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Pie, PieChart, RadialBar, RadialBarChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const DeshBoard = () => {
+    const data1 = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
+
     return (
-        <div>
-            <h1>deshBoard</h1>
+        <div className="container">
+            <div className="chart d-flex justify content-evenly">
+                <div className="line">
+                    <LineChart width={600} height={400} data={data1}>
+                        <Line dataKey={'sell'}></Line>
+                        <Line dataKey={'revenue'}></Line>
+                        <XAxis dataKey={'month'}></XAxis>
+                        <YAxis dataKey={'investment'}></YAxis>
+                    </LineChart>
+                </div>
+                <div>
+                    <AreaChart
+                        width={500}
+                        height={400}
+                        data={data1}
+                        margin={{
+                            top: 10,
+                            right: 30,
+                            left: 0,
+                            bottom: 0,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+                        <Area type="monotone" dataKey="sell" stroke="#8884d8" fill="#8884d8" />
+                    </AreaChart>
+                </div>
+
+            </div>
+            <div className="container d-flex justify content-evenly">
+            <div>
+                <BarChart width={400} height={400} data={data1}>
+                    <Bar dataKey="investment" fill="#8884d8" />
+                </BarChart>
+            </div>
+            <div>
+                <PieChart width={600} height={600}>
+                    <Pie data={data1} dataKey="sell" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+                    <Pie data={data1} dataKey="revenue" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+                </PieChart>
+            </div>
+            </div>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,8 +12,30 @@ const Home = () => {
                     <p>To the Muslims. the Quran is the Word of Godand contains <br></br>complete guidance for mankind.The Holy Quran is divided into <br></br>114 Surahs or Chapters and each Chapter consists of individual Ayaat or verses There are in total 6,666 verses in the Holy Quran.The Surahs are of varying lengths<br></br> But it also contains directives for its followers, <br></br>historical accounts of certain prophets and peoples</p>
                 </div>
                 <div className="image">
-                <img className=""  src="https://images.unsplash.com/photo-1580220810949-e7ddee6a4954?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60" alt="" />
+                    <img className="" src="https://images.unsplash.com/photo-1580220810949-e7ddee6a4954?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60" alt="" />
                 </div>
+            </div>
+            <div className="container">
+                <h1>Customer Review</h1>
+                <div className="review d-flex justify-content-evenly">
+                    <div>
+                        <h3>Name : Parvez Islam</h3>
+                        <p>The Quran Is a Book that Calms the Heart Of Man.it Spreads the Massage of Peace in The World by creating Fear of Allah  in the Heart and Touching the Prophet of Love</p>
+                        <h5>Ratings :</h5>
+                    </div>
+                    <div>
+                        <h3>Name : Mazharul Islam</h3>
+                        <p>The Quran Is a Book that Calms the Heart Of Man.it Spreads the Massage of Peace in The World by creating Fear of Allah  in the Heart and Touching the Prophet of Love</p>
+                        <h5>Ratings :</h5>
+                    </div>
+                    <div>
+                        <h3>Name : Papon Islam</h3>
+                        <p>The Quran Is a Book that Calms the Heart Of Man.it Spreads the Massage of Peace in The World by creating Fear of Allah  in the Heart and Touching the Prophet of Love</p>
+                        <h5>Ratings :</h5>
+                    </div>
+                </div>
+                <button>See More</button>
+
             </div>
         </div>
     );
